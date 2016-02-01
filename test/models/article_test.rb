0bcff9c6a04f4aceb8dd6350b72de22578c03b77
bernard laveaux <blaveaux@outlook.com>
2016-02-01 06:45:53 -0500

    expanded article timestamp validation test cases

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,8 +2,14 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   def setup
-    @user_with_podcasts = FactoryGirl.create :user_with_podcasts
-    @article = @user_with_podcasts.articles.build( content: "<body><p>html wrapped text</p></body>" )
+    # user with series containing podcasts
+    @user_with_series = FactoryGirl.create(:user_with_series, series_count: 3)
+    @article = @user_with_series.articles.build( content: "<body><p>html wrapped text</p></body>" )
+    @series_with_podcasts = @user_with_series.series.first
+    3.times do
+        podcast = @series_with_podcasts.podcasts.create (FactoryGirl.attributes_for :podcast)
+        podcast.timestamps.create FactoryGirl.attributes_for(:timestamp, podcast_end_time: podcast.end_time).merge(article: @article)
+    end
   end
 
   test "should be valid" do
@@ -28,6 +34,9 @@ class ArticleTest < ActiveSupport::TestCase
 
   test "should not be published when timestamp is not present" do
     @article.published = true
+    @series_with_podcasts.podcasts.each do |podcast|
+      podcast.timestamps.each {|timestamp| timestamp.update_attribute :article, nil}
+    end
     assert_not @article.valid?
     assert_match /Article cannot be published without an associated timestamp/, @article.errors[:base].to_s
   end
